2f5b4f0c4c484b7c5841db430f262872db550c6a
bastien03 <bastienmary@gmail.com>
2015-02-03 23:52:16 +0100

    wip

--- cors-server.py
+++ cors-server.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 
 import http.server
+import logging
 
 class CORSRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def end_headers(self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        super().end_headers()
+
+    def do_GET(self):
+        if self.path=="/cors":
+            logging.warning("--> CORS")
+            http.server.SimpleHTTPRequestHandler.send_header(self, 'Access-Control-Allow-Origin', '*')
+            http.server.SimpleHTTPRequestHandler.end_headers(self)
+        else:
+            logging.warning("--> no CORS")
+
+        #logging.warning("======= GET STARTED =======")
+        #logging.warning(self.headers)
+        self.send_response(207)
+        #self.wfile.write({"name":"Truffaut"})
+        http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 if __name__ == '__main__':
     http.server.test(HandlerClass=CORSRequestHandler)
